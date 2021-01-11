167e5a622df9a44e2713a7fa359cbc85e227538b
Corey Calhoun <corey.calhoun@dkgconceptsllc.com>
2021-01-11 14:45:46 -0600

    --- Auto-Git Commit ---

--- src/components/widgets/Widgets.js
+++ src/components/widgets/Widgets.js
@@ -1,14 +1,32 @@
 import React from 'react'
 import './Widgets.css'
 import InfoIcon from '@material-ui/icons/Info'
+import { FiberManualRecord } from '@material-ui/icons'
 
 function Widgets() {
+
+    const newsArticle = (heading, subtitle) => {
+        <div className="widgets__article">
+            <div className="widgets__articleLeft">
+                <FiberManualRecord />
+            </div>
+            <div className="widgets__articleRight">
+                <h4>{heading}</h4>
+                <p>{subtitle}</p>
+            </div>
+        </div>
+        
+    }
+
+
     return (
         <div className="widgets">
             <div className="widgets__header">
                 <h2>LinkedIn News</h2>
-                <InfoIcon />
+                <InfoIcon fontSize="small" />
             </div>
+
+            {newsArticle("this is a sample", 'Top News - 254 readers')}
         </div>
     )
 }
